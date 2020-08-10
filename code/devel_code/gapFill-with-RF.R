73dc4d278ee3d7f1fd34c65592f72155d6f89ff7
GregDuveiller <gduveiller@gmail.com>
2020-08-10 15:56:01 +0200

    Add capacity to switch gapfilling based between IGBPdet and IGBPgen

--- code/devel_code/gapFill-with-RF.R
+++ code/devel_code/gapFill-with-RF.R
@@ -18,7 +18,13 @@ require(here)
 #### Initialisation #### ---- 
 
 
-fig.path = 'data/inter_data/gapfilling_checkplots'
+iVar <- 'albedo'  # 'LSTday'  #  'albedo' #
+type <- 'IGBPdet' # 'IGBPgen'
+
+nu_thr <- 10
+minTHR <- 0.05
+
+fig.path <- paste0('data/inter_data/gapfilling_checkplots/', iVar, '_', type)
 
 #### load data #### ---- 
 source('data/input_data/___loadDataPaths___.R')  # dpath_s4t
@@ -39,17 +45,16 @@ load('data/inter_data/climData/df4ClimateSpace_1dd.RData') # dfClim
 
 #### Prepare masking to define extent #### ---- 
 
-# >>> NEED TO SWITCH BETWEEN DATASET (gen vs det)
+if(type == 'IGBPgen'){
 
-minTHR <- 0.05
-
-LCfile2 <- paste0('data/input_data',
-                  '/ESACCI-LC-L4-LCCS-Map-300m-P1Y-aggregated-1.000000Deg-2010-v2.0.7.nc')
-
-msk_FOR <- raster(LCfile2,varname='FOR') > minTHR
-msk_SHR <- raster(LCfile2,varname='SHR') > minTHR
-msk_CaG <- raster(LCfile2,varname='CaG') > minTHR
-msk_SAV <- raster(LCfile2,varname='SAV') > minTHR
+  LCfile <- paste0('data/input_data', '/CCI2', type,
+                   '/ESACCI-LC-L4-LCCS-Map-300m-P1Y-aggregated-1.000000Deg-2010-v2.0.7.nc')
+  
+  
+msk_FOR <- raster(LCfile, varname = 'FOR') > minTHR
+msk_SHR <- raster(LCfile, varname = 'SHR') > minTHR
+msk_CaG <- raster(LCfile, varname = 'CaG') > minTHR
+msk_SAV <- raster(LCfile, varname = 'SAV') > minTHR
 
 df_msk <- bind_rows(
   as.data.frame(msk_FOR | msk_SHR, xy = T) %>% 
@@ -68,13 +73,48 @@ df_msk <- bind_rows(
   rename(Lon = x, Lat = y, Mask = layer)
 
 df_msk$Mask[is.na(df_msk$Mask)] <- F  # because some had NAs due to msk sum
-
-
 nTrans <- length(unique(df_msk$Transition))
 
+}
 
 # 
 
+
+if(type == 'IGBPdet'){
+  
+  LCfile <- paste0('data/input_data', '/CCI2', type,
+                   '/ESACCI-LC-L4-LCCS-Map-300m-P1Y-aggregated-1.000000Deg-2010-v2.0.7.nc')
+  
+
+
+vgt.grps <- c('EBF','DBF','ENF','DNF','MF','SAV','SHR','GRA','CRO','WET')
+
+for(iVG in vgt.grps){
+  msk <- raster(LCfile, varname = iVG) > minTHR
+  eval(parse(text = paste0('msk_', iVG, ' <- msk')))
+}
+
+n <- length(vgt.grps); nTr <- n*(n-1)/2
+Tr.From <- vector(length = nTr); Tr.To <- Tr.From; i <- 1
+
+df_msk <- NULL
+iTrNum <- 1
+for(iPFTfrom in vgt.grps[1:(n-1)]){
+  for(iPFTto in vgt.grps[(1 + which(vgt.grps == iPFTfrom)):n]){
+
+    eval(parse(text = paste0('df <- as.data.frame(', 'msk_', iPFTfrom, 
+                             ' | msk_', iPFTto, ', xy = T)')))
+    df <- df %>% 
+      mutate(Transition = paste(iPFTfrom, iPFTto, sep = ' %=>% '), 
+             iTr = iTrNum) %>%
+      rename(Lon = x, Lat = y, Mask = layer)
+    df_msk <- df_msk %>% bind_rows(df)
+    iTrNum <- iTrNum + 1
+  }}
+
+df_msk$Mask[is.na(df_msk$Mask)] <- F  # because some had NAs due to msk sum
+nTrans <- length(unique(df_msk$Transition))
+}
 
 #### Define function to gap-fill a single layer #### ---- 
 
@@ -122,7 +162,7 @@ gapfill_layer <- function(df_dat, checkplot = T){
            cat_PPT_sum = factor(cut(PPT_sum, breaks = bin.PPT_sum, labels = F, include.lowest = T)),
            cat_AridIndex = factor(cut(AridIndex, breaks = bin.AridIndex, labels = F, include.lowest = T)),
            cat_SNW_sum = factor(cut(SNW_sum, breaks = bin.SNW_sum, labels = F, include.lowest = T))) %>%
-    right_join(by=c(paste0('cat_', c('TMP_mean','TMP_range','PPT_sum','AridIndex','SNW_sum'))),freq0)
+    right_join(by = c(paste0('cat_', c('TMP_mean','TMP_range','PPT_sum','AridIndex','SNW_sum'))),freq0)
   
   # set seed
   set.seed(1982)
@@ -213,98 +253,145 @@ gapfill_layer <- function(df_dat, checkplot = T){
 
 
 
-#### Wrap around a file #### ---- 
-
-iVar <- 'albedo' 
-type <- 'IGBPgen'
+#### Prepare run for a given NCDF file #### ---- 
 
 nc_filename_in <- paste0(dpath_s4t, '/', iVar,'_',type,'.nc')
-nc_filename_out <- paste0('data/final_data/', iVar, '_', type, '_gapfilled.nc')
+nc_filename_out <- paste0('data/final_data/', iVar, '_', type, '_gf.nc')
 
 nc <- nc_open(nc_filename_in)
+print(paste('Starting with', iVar, 'in', type, 'classification...'))
 
 
 
 # mk new nc
+comprLevel <- 7
+mzVal <- NA
+
+# Prepare the 4 dimensions
 dimX <- ncdim_def(name = 'lon',
                   units = ncatt_get(nc, varid = 'lon', attname = 'units')$value, 
                   vals = ncvar_get(nc, varid = 'lon'), unlim = F,
                   longname = ncatt_get(nc, varid = 'lon', attname = 'long_name')$value)
-
 dimY <- ncdim_def(name = 'lat',
                   units = ncatt_get(nc, varid = 'lat', attname = 'units')$value, 
                   vals = ncvar_get(nc, varid = 'lat'), unlim = F,
                   longname = ncatt_get(nc, varid = 'lat', attname = 'long_name')$value)
-
 dimM <- ncdim_def(name = 'mon',
                   units = ncatt_get(nc, varid = 'mon', attname = 'units')$value, 
                   vals = ncvar_get(nc, varid = 'mon'), unlim = F,
                   longname = ncatt_get(nc, varid = 'mon', attname = 'long_name')$value)
-
 dimT <- ncdim_def(name = 'iTr',
                   units = 'transition', 
                   vals = ncvar_get(nc, varid = 'iTr'), unlim = F,
                   longname = ncatt_get(nc, varid = 'iTr', attname = 'long_name')$value)
 
 
+# prepare new (gap-filled) var
+var_longname <- ncatt_get(nc, varid = paste0('Delta_', iVar), 
+                          attname = 'long_name')$value
 
-new_var <- ncvar_def(name = paste0('Delta_',iVar), 
-                     dim = list(dimX, dimY, dimM, dimT), 
-                     units = '', missval = NA, compression = 7,
-                     longname = ncatt_get(nc, varid = paste0('Delta_',iVar), 
-                                          attname = 'long_name')$value)
+new_var_GFD <- ncvar_def(name = paste0('Delta_', iVar, '_gapfilled'), 
+                         dim = list(dimX, dimY, dimM, dimT), 
+                         units = '', 
+                         missval = mzVal, compression = comprLevel,
+                         longname = paste0('Gap-filled ', 
+                                           tolower(substr(var_longname, 0, 1)),
+                                           substring(var_longname, 2)))
 
+varID <- paste0('Delta_', iVar)
+new_var_delta <- ncvar_def(name = varID, 
+                           dim = list(dimX, dimY, dimM, dimT), 
+                           units = '', 
+                           missval = mzVal, compression = comprLevel,
+                           longname =  ncatt_get(nc, varid = varID, 
+                                                 attname = 'long_name')$value)
+
+varID <- paste0('SD_Delta_', iVar)
+new_var_sd <- ncvar_def(name = varID, 
+                        dim = list(dimX, dimY, dimM, dimT), 
+                        units = '', 
+                        missval = mzVal, compression = comprLevel,
+                        longname =  ncatt_get(nc, varid = varID, 
+                                              attname = 'long_name')$value)
+
+varID <- paste0('N_Delta_', iVar)
+new_var_n <- ncvar_def(name = varID, 
+                       dim = list(dimX, dimY, dimM, dimT), 
+                       units = '', 
+                       missval = mzVal, compression = comprLevel,
+                       longname =  ncatt_get(nc, varid = varID, 
+                                             attname = 'long_name')$value)
+
+
+
+# create new nc
 nc_new <- nc_create(filename = nc_filename_out,
-                    vars = new_var, force_v4 = T, verbose = F)
-
+                    vars = list(new_var_GFD, new_var_delta, new_var_sd, new_var_n), 
+                    force_v4 = T, verbose = F)
 nc_close(nc_new)
 
 
 
 
+#### Wrap around a file #### ---- 
+
+
 nc_new <- nc_open(nc_filename_out, write = TRUE)
 
-iMonth <- 1
-iTrans <- 2
-
-for(iTrans in 2:nTrans){
-for(iMonth in 1:12){
+for(iTrans in 1:nTrans){
+  for(iMonth in 1:12){
+    
+    # get the original delta data ...
+    nc_dat <- ncvar_get(nc, varid = paste0('Delta_',iVar), 
+                        start = c(1,1, iMonth, iTrans), count = c(-1, -1, 1, 1))
+    # ... and pass it to the new NetCDF
+    ncvar_put(nc = nc_new, varid = paste0('Delta_',iVar), vals = nc_dat,
+              start = c(1,1, iMonth, iTrans), count = c(-1, -1, 1, 1))
+    
+    
+    # get the sd delta data ...
+    sd_dat <- ncvar_get(nc, varid = paste0('SD_Delta_',iVar),
+                        start = c(1,1, iMonth, iTrans), count = c(-1, -1, 1, 1))
+    # ... and pass it to the new NetCDF
+    ncvar_put(nc = nc_new, varid = paste0('SD_Delta_',iVar), vals = sd_dat,
+              start = c(1,1, iMonth, iTrans), count = c(-1, -1, 1, 1))
+    
+    
+    # get the N delta data
+    nu_dat <- ncvar_get(nc, varid = paste0('N_Delta_',iVar), 
+                        start = c(1,1, iMonth, iTrans), count = c(-1, -1, 1, 1))
+    # ... and pass it to the new NetCDF
+    ncvar_put(nc = nc_new, varid = paste0('N_Delta_',iVar), vals = nu_dat,
+              start = c(1,1, iMonth, iTrans), count = c(-1, -1, 1, 1))    
+    
+    
   
-# get the data 
-nc_dat <- ncvar_get(nc, varid = paste0('Delta_',iVar), 
-                    start = c(1,1, iMonth, iTrans), count = c(-1, -1, 1, 1))
-
-
-# set a threshold on quality based on standard deviation within gridcell
-sd_thr <- 0.05
-sd_dat <- ncvar_get(nc, varid = paste0('SD_Delta_',iVar), 
-                    start = c(1,1, iMonth, iTrans), count = c(-1, -1, 1, 1))
-
-# set a threshold on quality based on number of 0.05dd samples within gridcell
-nu_thr <- 5
-nu_dat <- ncvar_get(nc, varid = paste0('N_Delta_',iVar), 
-                    start = c(1,1, iMonth, iTrans), count = c(-1, -1, 1, 1))
-
-nc_dat[sd_dat > sd_thr] <- NaN
-nc_dat[nu_dat < nu_thr] <- NaN
-
-
-df_dat <- dfClim %>% 
-  mutate(delta = as.vector(nc_dat),
-         month = iMonth) %>%
-  left_join(df_msk %>% filter(iTr == iTrans), by = c('Lon', 'Lat')) %>%
-  filter(Mask == T)
-
-df_dum <- gapfill_layer(df_dat)
-
-df_out <- dfClim %>% select(Lon, Lat) %>% 
-  left_join(df_dum, by = c("Lon", "Lat"))
-
-ncvar_put(nc = nc_new, varid = paste0('Delta_',iVar), 
-          vals = df_out$delta,
-          start = c(1,1, iMonth, iTrans), count = c(-1, -1, 1, 1), verbose = F)
-
-}
+    # filter out points that have few underlying samples
+    nc_dat[nu_dat < nu_thr] <- NaN
+    
+    # prepare the data into a proper dataframe
+    df_dat <- dfClim %>% 
+      mutate(delta = as.vector(nc_dat),
+             month = iMonth) %>%
+      left_join(df_msk %>% filter(iTr == iTrans), by = c('Lon', 'Lat')) %>%
+      filter(Mask == T)
+    
+    # gap-fill where possible
+    df_dum <- gapfill_layer(df_dat)
+    
+    # fill extra points with no data
+    df_out <- dfClim %>% select(Lon, Lat) %>% 
+      left_join(df_dum, by = c("Lon", "Lat"))
+    
+    # put the new variable inside the new NetCDF
+    ncvar_put(nc = nc_new, 
+              varid = paste0('Delta_', iVar, '_gapfilled'), 
+              vals = df_out$delta,
+              start = c(1,1, iMonth, iTrans), count = c(-1, -1, 1, 1))
+    
+    
+  }
+  print(paste(' >>>', iVar, 'transition', iTrans, '/', nTrans, 'is done'))
 }
 
 nc_close(nc_new)
